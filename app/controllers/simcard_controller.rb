c66afdd440774aa877b5f57dc595e2035a7b8023
sebas <sevlin2107@gmail.com>
2021-11-28 17:25:52 -0300

    controller simcard creado

--- app/controllers/simcard_controller.rb
+++ app/controllers/simcard_controller.rb
@@ -1,2 +1,41 @@
 class SimcardController < ApplicationController
+    #create simcard controller
+    def create
+        @simcard = Simcard.new(simcard_params)
+        if @simcard.save
+            render json: @simcard, status: :created
+        else
+            render json: @simcard.errors, status: :unprocessable_entity
+        end
+    end
+    #update simcard controller
+    def update
+        @simcard = Simcard.find(params[:id])
+        if @simcard.update(simcard_params)
+            render json: @simcard
+        else
+            render json: @simcard.errors, status: :unprocessable_entity
+        end
+    end
+    #delete simcard controller
+    def destroy
+        @simcard = Simcard.find(params[:id])
+        @simcard.destroy
+        head :no_content
+    end
+    #show simcard controller
+    def show
+        @simcard = Simcard.find(params[:id])
+        render json: @simcard
+    end
+    #list simcard controller
+    def index
+        @simcards = Simcard.all
+        render json: @simcards
+    end
+    private
+    #simcard params
+    def simcard_params
+        params.require(:simcard).permit(:number, :consumption, :balance, :client_id)
+    end
 end
