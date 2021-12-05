128058d31739770fc76281e1247d5ae60cfc06ce
sebas <sevlin2107@gmail.com>
2021-12-05 20:53:53 -0300

    vistas y login regular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-
     protect_from_forgery with: :exception
 
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    helper_method :current_client
+
+
+
     def current_ability
      @current_ability ||= Ability.new(current_user)
    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/dashboard'
+
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :clients
+
+  resources :home
+
+  root 'home#index'
+
+  get '/client' => "home#dashboard", :as => :client_root
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
